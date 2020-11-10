3f8064b85718f378a5b386fee6d192be72df796a
pony <383385741@qq.com>
2020-11-10 12:32:18 +0800

    add aarch64

--- linux/x86-64/test.go
+++ linux/x86-64/test.go
@@ -20,21 +20,21 @@ func main() {
         "access_key": access_key,
         "access_secret": access_secret,
         "host": host,
-        "recognize_type": acrcloud.ACR_OPT_REC_HUMMING,
+        "recognize_type": acrcloud.ACR_OPT_REC_AUDIO,
     }
 
     start := time.Now().UnixNano() / 1e6
     fmt.Println(time.Now())
     var recHandler = acrcloud.NewRecognizer(configs)
 
-    userParams := map[string]string {
-        "title": "title",
-        "artist": "artist",
-        "lyrics": "lyrics",
-    }
+    //userParams := map[string]string {
+    //    "title": "title",
+    //    "artist": "artist",
+    //    "lyrics": "lyrics",
+    //}
 
-    //result, err := recHandler.RecognizeByFileBuffer(b, 0, 20, nil)
-    result, err := recHandler.RecognizeByFileBuffer(b, 0, 30, userParams)
+    result, err := recHandler.RecognizeByFileBuffer(b, 0, 20, nil)
+    //result, err := recHandler.RecognizeByFileBuffer(b, 0, 30, userParams)
 
     // WAVE audio, Microsoft PCM, 16 bit, mono 8000 Hz
     //result, err := recHandler.Recognize(b, userParams)
